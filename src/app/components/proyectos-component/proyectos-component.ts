d583431c32501d498204c3a6904f5dc40f9e97ad
Luiggyn <luiggy@correo.com>
2026-01-21 10:27:53 -0500

    fix: proyectos

--- src/app/components/proyectos-component/proyectos-component.ts
+++ src/app/components/proyectos-component/proyectos-component.ts
@@ -4,11 +4,12 @@ import { NgxFadeComponent } from '@omnedia/ngx-fade';
 import { ProyectoCardComponent } from '../proyecto-card/proyecto-card.component';
 import { Proyecto } from '../../interfaces/proyecto.interface';
 import { NgxVortexComponent } from '@omnedia/ngx-vortex';
+import { NgxDarkVeilComponent } from "@omnedia/ngx-dark-veil";
 
 @Component({
      selector: 'app-proyectos',
      standalone: true,
-     imports: [CommonModule, NgxFadeComponent, ProyectoCardComponent, NgxVortexComponent],
+     imports: [CommonModule, ProyectoCardComponent, NgxDarkVeilComponent, NgxFadeComponent, NgxVortexComponent],
      templateUrl: './proyectos-component.html',
      styleUrl: './proyectos-component.css'
 })
