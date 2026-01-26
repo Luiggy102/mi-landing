c8462f38db72b9f2d2830b3f38828d7c171da1aa
Luiggyn <luiggy@correo.com>
2026-01-26 10:00:24 -0500

    add: data de experiencia

--- src/app/data/experiencia.data.ts
+++ src/app/data/experiencia.data.ts
@@ -8,38 +8,74 @@ export class ExperienciaData {
 
   public experienciasData: Experiencia[] = [
     {
-      nombreEmpresa: 'Macrobills Solutions',
+      nombreEmpresa: 'NetEvolution S.A.S',
       nombreCargo: 'Fullstack Developer',
-      mesInicio: 'Agosto',
+      mesInicio: 'Junio',
       anoInicio: 2025,
       mesFin: 'Presente',
       anoFin: null,
       actividadesRealizadas: [
         {
-          descripcion: 'Desarrollo de módulos de nómina y recursos humanos adaptados a la legislación ecuatoriana.',
-          tecnologias: ['Angular', 'Laravel', 'SQL Server']
+          descripcion: 'Desarrollo a nivel de frontend y backend de plataforma de Guías de remisión',
+          tecnologias: ['Angular', 'TypeScript', 'ASP.NET', 'Entity Framework', 'C#', 'JWT', 'Azure Data Studio', 'SQL Server']
         },
         {
-          descripcion: 'Integración de asientos contables automáticos para procesos de finiquitos.',
-          tecnologias: ['PHP', 'C#']
+          descripcion: 'Mantenimiento y Desarrollo frontend y backend para plataforma crypto MVP',
+          tecnologias: ['React', 'Express.JS', 'MongoDB', 'JavaScript', 'APIs']
+        },
+        {
+          descripcion: 'Desarrollo fullstack para software contable',
+          tecnologias: ['Laravel', 'php', 'MVC', 'Middlewares', 'MySQL']
+        },
+        {
+          descripcion: 'Solución de Problemas y Conexión a servidores para conﬁguraciones en entornos de producción',
+          tecnologias: ['FTP', 'SFTP', 'WHM', 'SSH', 'SSH Tunel', 'Bash']
+        },
+        {
+          descripcion: 'Manejo de sistemas Linux para desarrollo',
+          tecnologias: ['Fedora Workstation 42']
+        },
+        {
+          descripcion: 'Manejo de versiones',
+          tecnologias: ['Git']
         }
       ]
     },
     {
-      nombreEmpresa: 'Logistics & WMS Corp',
-      nombreCargo: 'Backend Developer',
-      mesInicio: 'Enero',
-      anoInicio: 2024,
-      mesFin: 'Julio',
+      nombreEmpresa: 'Grupo A&G',
+      nombreCargo: 'FullStack Developer',
+      mesInicio: 'Abril',
+      anoInicio: 2025,
+      mesFin: 'Junio',
       anoFin: 2025,
       actividadesRealizadas: [
         {
-          descripcion: 'Optimización de algoritmos de Cross-Docking para la gestión de almacenes.',
-          tecnologias: ['ASP.NET Core', 'Entity Framework']
+          descripcion: 'Diseño, desarrollo y mantenimiento de plataforma de calendarios para eventos internos',
+          tecnologias: ['Angular']
         },
         {
-          descripcion: 'Diseño de arquitectura de base de datos para seguimiento de inventario en tiempo real.',
-          tecnologias: ['SQL Server']
+          descripcion: 'Mantenimiento y desarrollo de features a plataforma de gestión de torneos de Poker',
+          tecnologias: ['Laravel', 'Blade Templates', 'Laravel sanctum']
+        },
+        {
+          descripcion: 'Implementación de chatbot con IA en web para servicio al cliente',
+          tecnologias: ['Tawk.to', 'JavaScript']
+        },
+        {
+          descripcion: 'Gestión de bases de datos para todos los proyectos',
+          tecnologias: ['MySQL', 'phpMyAdmin']
+        },
+        {
+          descripcion: 'Creación de contenedores de los proyectos para fácil despliegue',
+          tecnologias: ['Docker', 'Bash']
+        },
+        {
+          descripcion: 'Manejo de sistemas Linux para desarrollo',
+          tecnologias: ['Ubuntu Desktop']
+        },
+        {
+          descripcion: 'Despliegue a producción',
+          tecnologias: ['CPanel']
         }
       ]
     }
